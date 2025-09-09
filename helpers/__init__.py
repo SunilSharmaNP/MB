0a1fc78c5b8fd6cf394cf6d197ac55da96be4659
Sunil Sharma <Arnil1718@gmail.com>
2025-09-09 09:20:05 +0545

    Update __init__.py

--- helpers/__init__.py
+++ helpers/__init__.py
@@ -1,1 +1,23 @@
+# helpers/__init__.py
+"""Helper modules for AdvancedMergeBot"""
 
+__version__ = "1.0.0"
+__author__ = "AdvancedMergeBot"
+
+# Import all helper functions for easy access
+from .database import database
+from .downloader import download_file_from_url, download_telegram_file
+from .merger import merge_videos, merge_video_with_audio, merge_video_with_subtitles
+from .uploader import upload_to_telegram, GofileUploader, dual_upload
+
+__all__ = [
+    'database',
+    'download_file_from_url',
+    'download_telegram_file', 
+    'merge_videos',
+    'merge_video_with_audio',
+    'merge_video_with_subtitles',
+    'upload_to_telegram',
+    'GofileUploader',
+    'dual_upload'
+]
